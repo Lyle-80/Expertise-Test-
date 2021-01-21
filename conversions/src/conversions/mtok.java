a1695711807f6245a6d76bc62437149977837695
Lyle-80 <47191439+Lyle-80@users.noreply.github.com>
2021-01-21 15:06:25 +0200

    Update mtok.java

--- conversions/src/conversions/mtok.java
+++ conversions/src/conversions/mtok.java
@@ -10,28 +10,28 @@ public class mtok {
 		
 // TODO Auto-generated method stub
 		
-// Start of execution duration log
+		// Start of execution duration log
 		long startTime=System.currentTimeMillis();
 		
 			
 		
-// Declare "mls" & "kms" variables
+			// Declare "mls" & "kms" variables
 			double mls, kms;
 
-// Getting user input using Scanner
+			// Getting user input using Scanner
 			System.out.println("Welcome ('',) Please input distance in "
 					+ "Mile's bellow and hit ENTER:");
 			@SuppressWarnings("resource")
 			Scanner input = new Scanner(System.in);
 			mls = input.nextDouble();
 
-// Conversion & OutPut
+			// Conversion & OutPut
 			kms = mls * 1.6;
 			System.out.println("Thank,you!!! Distance in Kilometer's is: "+ kms + "kms");
 			
 		
 			
-// End of Execution Duration log:
+		// End of Execution Duration log:
 	        long endTime=System.currentTimeMillis();
 	        System.out.println("Execution Duration: " + (endTime-startTime) + "'milliseconds'");
 	}
